f9b02cf29b8e2fe2e5ccfff3726ac0657f210c57
aswin890 <112414779+aswin890@users.noreply.github.com>
2024-02-08 23:31:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <script type="module" src="/dist/main.js"></script>
-
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
